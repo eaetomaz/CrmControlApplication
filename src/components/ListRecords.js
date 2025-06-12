cb982f660dc139c1e5a6b76e4a9002d0741b28fd
Guilherme Tomaz <guilhermetomaz.bsi@gmail.com>
2025-06-12 19:52:24 -0300

    feat: btn de atualizar registro e estilização básico dos botões

--- src/components/ListRecords.js
+++ src/components/ListRecords.js
@@ -8,17 +8,35 @@ export async function CreateList(list) {
     listCrm.forEach(crm => {
         
         const li = document.createElement('li');
-        li.textContent = crm.cliente + ' | ' + crm.celular;        
+        li.style.display = 'flex';
+        li.style.alignItems = 'center';        
+
+        li.innerHTML = crm.cliente + '<br>Celular: ' + crm.celular +
+         '<br>Email: ' + crm.email +
+         '<br>Motivo: ' + crm.motivo +
+         '<br>Endereço: ' + crm.endereco;        
 
         const btnDelete = document.createElement('button');
-        btnDelete.textContent = 'Apagar';
+        btnDelete.className = 'btn-delete';
+        btnDelete.style.marginLeft = 'autot';
+        btnDelete.textContent = 'Apagar';        
         btnDelete.onclick = async() => {
             await DeleteRecord(crm.id);
             await CreateList(list);
 
         };
-        
-        li.appendChild(btnDelete);        
+
+        const btnAtt = document.createElement('button');
+        btnAtt.className = 'btn-att';
+        btnAtt.style.marginLeft = 'auto';
+        btnAtt.textContent = 'Atualizar';
+        btnAtt.onclick = async() => {
+            await AttRecord(data);
+            await CreateList(list);
+        };
+                            
+        li.appendChild(btnAtt);
+        li.appendChild(btnDelete);    
         list.appendChild(li);
     });    
 
